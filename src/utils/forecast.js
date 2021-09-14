e8a607b295e0cabe450e30df6304d1f831222e18
Ray <uttam-kumar.ray@siemens-healthineers.com>
2021-09-14 16:30:03 +0530

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined)
         } else {
             callback(undefined, body.current.weather_descriptions[0] + '. It is crurrently ' + body.current.temperature + 
-        ' degrees out. It feels like ' + body.current.feelslike +' degrees out.')
+        ' degrees out. It feels like ' + body.current.feelslike +' degrees out. Humidity is ' + body.current.humidity +'%.')
         }
     })
 }
